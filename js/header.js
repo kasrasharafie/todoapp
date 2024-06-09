f4c3ae21d2e2f024df5ad92e3512520183b80d44
kasrasharafie <kasra.sharafie@gmail.com>
2024-06-09 23:05:56 +0330

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,44 @@
+
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const crossIcon = document.querySelector('.cross-icon');
+const showMoreMenu = document.querySelector('#show-more-menu');
+const showProductMenu = document.querySelector('#show-product-menu');
+
+hamburgerIcon.addEventListener('click', function () {
+  hamburgerMenu.classList.add('show-hamburger-menu');
+});
+
+crossIcon.addEventListener('click', function () {
+  hamburgerMenu.classList.remove('show-hamburger-menu');
+});
+
+showMoreMenu.addEventListener('mouseenter', function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.animation = 'showUp 0.35s 1';
+  ul.style.display = 'block';
+});
+
+showMoreMenu.addEventListener('mouseleave', function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.display = 'none';
+});
+
+showProductMenu.addEventListener('mouseenter', function (e) {
+  const div = e.target.childNodes[5];
+  div.style.animation = 'showUp 0.35s 1';
+  div.style.display = 'flex';
+});
+
+showProductMenu.addEventListener('mouseleave', function (e) {
+  const div = e.target.childNodes[5];
+  div.style.display = 'none';
+});
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth >= 768) { // 768px به عنوان حد برای دستگاه‌های موبایل
+    var ul = document.getElementById("a");
+    while(ul.firstChild) ul.removeChild(ul.firstChild);
+  }
+});
+
